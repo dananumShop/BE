bbf3862d97f6cacb043cefea29934a9595f3312d
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-06-26 18:16:22 +0900

    initial commit
    기본 프로젝트 생성을 하였습니다.

---
+++ src/main/java/org/dananum/dananum_shop/DananumShopApplication.java
@@ -0,0 +1,13 @@
+package org.dananum.dananum_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DananumShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DananumShopApplication.class, args);
+    }
+
+}
