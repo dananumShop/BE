91aa9303aaa81d32e7b79f6d64e27ffe46d177d4
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-06-28 23:35:24 +0900

    fix: 배포 파일 이름 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ REPOSITORY=/home/ubuntu/app
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fla java | grep shopping-mall | awk '{print $1}')
+CURRENT_PID=$(pgrep -fla java | grep dananum_shop | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
