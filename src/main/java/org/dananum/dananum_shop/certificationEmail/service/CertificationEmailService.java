f9aae6524988281d0228dc701a6ad10477605b1f
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-07-02 22:16:27 +0900

    feat: yml update

--- src/main/java/org/dananum/dananum_shop/certificationEmail/service/CertificationEmailService.java
+++ src/main/java/org/dananum/dananum_shop/certificationEmail/service/CertificationEmailService.java
@@ -1,4 +1,4 @@
-package org.dananum.dananum_shop.certificationEmail.web.service;
+package org.dananum.dananum_shop.certificationEmail.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
