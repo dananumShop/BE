bda0a1bfdab4b1add7a67de08592168093e8e90a
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-07-02 22:04:25 +0900

    feat: email duplicate
    이메일 중복 테스트

--- src/main/java/org/dananum/dananum_shop/certificationEmail/web/controller/CertificationEmailController.java
+++ src/main/java/org/dananum/dananum_shop/certificationEmail/web/controller/CertificationEmailController.java
@@ -1,5 +1,7 @@
 package org.dananum.dananum_shop.certificationEmail.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.dananum.dananum_shop.certificationEmail.web.dto.EmailCheckReqDto;
@@ -15,17 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/public/mail")
+@Tag(name = "회원관련 API")
 public class CertificationEmailController {
 
     private final CertificationEmailService certificationEmailService;
 
-
+    @Operation(summary = "이메일 요청", description = "이메일 인증에 필요한 인증번호를 받는 api입니다.")
     @PostMapping("/send-mail")
     public String sendMail(@RequestBody EmailReqDto emailReqDto){
         log.info("이메일 인증 이메일 : {}", emailReqDto.getEmail());
         return certificationEmailService.joinEmail(emailReqDto);
     }
 
+    @Operation(summary = "이메일 확인", description = "이메일로 받은 인증번호를 확인하는 api입니다.")
     @PostMapping("/check-auth-num")
     public String checkAuthNum(@RequestBody EmailCheckReqDto emailCheckReqDto) {
         Boolean checked = certificationEmailService.checkAuthNum(emailCheckReqDto.getEmail(), emailCheckReqDto.getAuthNum());
--- src/main/java/org/dananum/dananum_shop/user/web/controller/AuthController.java
+++ src/main/java/org/dananum/dananum_shop/user/web/controller/AuthController.java
@@ -11,10 +11,7 @@ import org.dananum.dananum_shop.user.web.dto.login.LoginReqDto;
 import org.dananum.dananum_shop.user.web.dto.signup.SignupReqDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -42,5 +39,14 @@ public class AuthController {
         log.debug("[AUTH] 로그인이 성공적으로 이루어 졌습니다.");
 
         return ResponseEntity.ok(CommonResponseDto.successResponse("로그인 성공"));
+    }
+
+    @Operation(summary = "이메일 중복 테스트", description = "이메일이 중복되었는지 확인하는 api입니다.")
+    @GetMapping("/duplicate-test/email")
+    public  ResponseEntity<CommonResponseDto> emailDuplicateTest(@RequestParam String userEmail) {
+        log.debug("[DUPLICATE] 이메일 중복확인 요청이 들어왔습니다.");
+        authService.emailDuplicateTest(userEmail);
+        log.debug("[DUPLICATE] 이메일 중복검사가 정상적으로 이루어 졌습니다.");
+        return ResponseEntity.ok().body(CommonResponseDto.successResponse("사용가능한 이메일입니다."));
     }
 }
