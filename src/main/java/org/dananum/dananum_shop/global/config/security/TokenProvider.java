78d9e65421b94de28862b2e88b5bdf9d4faf5bd9
ysk9926 <74546856+ysk9926@users.noreply.github.com>
2024-07-15 14:45:58 +0900

    fix: undo

--- src/main/java/org/dananum/dananum_shop/global/config/security/TokenProvider.java
+++ src/main/java/org/dananum/dananum_shop/global/config/security/TokenProvider.java
@@ -75,7 +75,6 @@ public class TokenProvider {
         Cookie accessTokenCookie = new Cookie("ACCESS_TOKEN", accessToken);
         accessTokenCookie.setPath("/");
         accessTokenCookie.setMaxAge(ACCESS_TOKEN_EXPIRE_TIME);
-        accessTokenCookie.setDomain("localhost");
 
 
         httpServletResponse.addCookie(accessTokenCookie);
